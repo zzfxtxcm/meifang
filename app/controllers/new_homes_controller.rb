6c106dd6eca0b2365704a53dc06455cf63cb0784
Wu Zhi Jie <wzj_12@163.com>
2014-03-30 22:53:03 +0800

    add information search

--- app/controllers/new_homes_controller.rb
+++ app/controllers/new_homes_controller.rb
@@ -3,6 +3,19 @@ class NewHomesController < ApplicationController
     @new_homes = NewHome.order('created_at DESC')
                         .paginate(page: params[:page])
                         .per_page(5)
+    @keyword = Sunspot.search(NewHome) do
+      keywords params[:keyword]
+      order_by :created_at, :desc
+      paginate :page => params[:page], :per_page => 5
+    end
+
+    @new_homes = @keyword.results
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @new_homes }
+      format.js
+    end
   end
 
   def show
--- app/helpers/new_homes_helper.rb
+++ app/helpers/new_homes_helper.rb
@@ -2,11 +2,13 @@ module NewHomesHelper
 
   def new_homes_dropdown(items)
     result = []
-    if !items.nil?
+
+    if present?
       items.map do |item|
         result << [PinYin.of_string(item.name)[0][0].upcase.to_s + " - " + item.name, item.id]
       end
     end
+    
     result.sort
   end
 
