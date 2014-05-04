3d415c4528bb106ef021c7d1bec694505e425878
Wu Zhi Jie <wzj_12@163.com>
2014-05-04 08:53:05 +0800

    tmp

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20140429030708) do
-=======
 ActiveRecord::Schema.define(version: 20140430071529) do
->>>>>>> develop
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
