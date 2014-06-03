b17ef9a2ac65aca84ec2437c2f9b76e4d03a538b
Wu Zhi Jie <wzj_12@163.com>
2014-06-03 16:22:21 +0800

    bug

--- app/controllers/new_homes_controller.rb
+++ app/controllers/new_homes_controller.rb
@@ -8,6 +8,9 @@ class NewHomesController < ApplicationController
                         .per_page(5)
 
     @keyword = Sunspot.search(NewHome) do
+      per_page = params[:per_page]
+      per_page = 5 if per_page.blank?
+
       keywords params[:keyword]
       with(:area_id).equal_to(params[:area_id]) if params[:area_id].present?
       with(:status_id).equal_to(params[:status_id]) if params[:status_id].present?
@@ -16,7 +19,7 @@ class NewHomesController < ApplicationController
       with(:property_type_id).equal_to(params[:property_type_id]) if params[:property_type_id].present?
       with(:price).between(params[:mix]..params[:max]) if params[:mix].present? && params[:max].present?
       order_by :created_at, :desc
-      paginate :page => params[:page], :per_page => (params[:per_page].present? || 5)
+      paginate :page => params[:page], :per_page => per_page
     end
 
     @new_homes = @keyword.results
