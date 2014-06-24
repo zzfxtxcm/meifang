69c3727c36be7e02abeaf30458a68220edf415aa
Wu Zhi Jie <wzj_12@163.com>
2014-06-25 00:48:27 +0800

    tmp

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,6 @@
 require "redis"
 require "redis-namespace"
 require "redis/objects"
-require "uri"
 
 redis_server = Rails.application.secrets.redis_server
 redis_port = Rails.application.secrets.redis_port
@@ -10,12 +9,14 @@ redis_namespace = Rails.application.secrets.redis_namespace
 
 Redis::Objects.redis = Redis.new(host: redis_server, port: redis_port)
 
-sidekiq_url = URI.parse(ENV["REDISTOGO_URL"] || "redis://#{redis_server}:#{redis_port}/#{redis_db_num}")
+sidekiq_url = "redis://#{redis_server}:#{redis_port}/#{redis_db_num}"
 
 Sidekiq.configure_server do |config|
+  config.logger.level = Logger::ERROR
   config.redis = { url: sidekiq_url, namespace: redis_namespace }
 end
 
 Sidekiq.configure_client do |config|
+  config.logger.level = Logger::ERROR  
   config.redis = { url: sidekiq_url, namespace: redis_namespace }
 end
